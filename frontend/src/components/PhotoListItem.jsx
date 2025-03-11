0748116fe956c05435627c14e3bf3e9c6b2fc2fb
Tiago Plaisant <tiagoplaisant@hotmail.com>
2025-03-11 16:52:36 -0600

    adding style to photoListItem(2)

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,15 +6,18 @@ const PhotoListItem = (props) => {
 
   return (
     <>
-    <div class="photo-list__item">
-      <img src={urls.regular}/>
+    <div className ="photo-list__item">
+      <img className ="photo-list__image" src={urls.regular}/>    
+      <div className="photo-list__user-details">      
+        <img className ="photo-list__user-profile" src={user.profile}/>
+        <div className="photo-list__user-info">
+          {user.name}
+          <div className="photo-list__user-location">
+            {location.city}, {location.country}
+          </div>
+        </div>
+      </div>      
     </div>    
-    <img src={user.profile}/>
-    <br></br>
-    {user.name}
-    <br></br>
-    {location.city}  {""}
-    {location.country}
     </>
   )
 };
